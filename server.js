fb9e405907e8ca89464c6d276d4a6ce2ab1b7b9b
steevenortega <steevenortega@gmail.com>
2023-09-21 16:55:50 -0500

    Terminacion backend proyecto tasks

--- server.js
+++ server.js
@@ -1,13 +1,51 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const jwt = require('jsonwebtoken')
+const userModel = require('./models/User')
 const TaskRoutes = require('./routes/TaskRoutes')
 const PORT = 3000
 
+const SECRET_KEY ='HOLA MUNDO'
+
 const server = express()
+
 
 server.use(express.json())
 
-server.use('/api/v1/tasks', TaskRoutes)
+server.use('/api/v1/auth', (req, res) => {
+    const { email, password } = req.body
+  
+    const validatedUser = userModel.validateUser(email, password)
+  
+    if (validatedUser) {
+      const token = jwt.sign({ validatedUser }, SECRET_KEY, { expiresIn: '5m' })
+      res.status(201).send({ token })
+    } else {
+      res.status(401).send({ message: 'Correo o contraseña incorrectos' })
+    }
+  
+  })
+
+  const validateToken = (req, res, next) => {
+    const authHeader = req.get('authorization')
+  
+    if (authHeader) {
+      // Bearer <token>
+      const accessToken = authHeader.split(' ')[1]
+  
+      jwt.verify(accessToken, SECRET_KEY, (error, decode) => {
+        if (error) {
+          res.status(401).send({ message: 'Token no valido' })
+        } else {
+          next()
+        }
+      })
+    } else {
+      res.status(404).send({message: 'Petición incorrecta'})
+    }
+  }
+  
+  server.use('/api/v1/tasks', validateToken, TaskRoutes)
 
 const mongooseConnect = async () => {
     try{
